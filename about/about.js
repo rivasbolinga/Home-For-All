0c0142e7b494bac9f4d8a608fec5b142cf4723d8
Andrea <andrearivas4cinfor@gmail.com>
2022-11-29 19:52:50 +0100

    Fix linters

--- about/about.js
+++ about/about.js
@@ -1,24 +1,23 @@
-'use script'
+'use script';
 
-//Open mobile menu
+// Open mobile menu
 
 const mobileMenu = document.querySelector('.mobile-menu');
 const btnOpenMenu = document.querySelector('.hamburger-menu');
 
-
-const openMenu = function() {
-  mobileMenu.style.display="flex";
-}
+const openMenu = function () {
+  mobileMenu.style.display = 'flex';
+};
 btnOpenMenu.addEventListener('click', openMenu);
 
-//Close mobile menu
+// Close mobile menu
 
 const btnCloseMenu = document.querySelector('.btn-close-l');
 const linkSection = document.querySelector('.links');
 
-const closeMenu = function() {
-  mobileMenu.style.display = "none";
-}
+const closeMenu = function () {
+  mobileMenu.style.display = 'none';
+};
 
-btnCloseMenu.addEventListener("click", closeMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
 linkSection.addEventListener('click', closeMenu);
--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-'use script'
+'use script';
 
-const speakerBoxes = document.querySelector(".speaker-boxes");
+const speakerBoxes = document.querySelector('.speaker-boxes');
 
-//ARRAY of speakers:
+// ARRAY of speakers:
 
 const speakers = [
   {
-    name: "Roberto Sanchez",
-    image: "../assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_178884984-stock-photo-teacher.jpg",
-    profession: "Professor of Law at the University of London",
-    resume: "Roberto is a lecturer in law, and has worked at Newcastle Law School, as well as London, since 2016.",
+    name: 'Roberto Sanchez',
+    image: '../assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_178884984-stock-photo-teacher.jpg',
+    profession: 'Professor of Law at the University of London',
+    resume: 'Roberto is a lecturer in law, and has worked at Newcastle Law School, as well as London, since 2016.',
   },
   {
-    image: "../assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_31934889-stock-photo-smiling-teacher-standing-in-classroom.jpg",
-    name: "Maura Elliot",
-    profession: "Professor of Law at the University of Essex (UK)",
-    resume: "Maura is a senior Lecturer at the School of Law of the University of Essex (UK)",
+    image: '../assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_31934889-stock-photo-smiling-teacher-standing-in-classroom.jpg',
+    name: 'Maura Elliot',
+    profession: 'Professor of Law at the University of Essex (UK)',
+    resume: 'Maura is a senior Lecturer at the School of Law of the University of Essex (UK)',
   },
   {
-    name: "Carla Fernandez",
-    image: "./assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_39309469-stock-photo-beautiful-african-american-student-or.jpg",
-    profession: "Professor of Law at the University of Madrid",
-    resume: "Carla specializes in the rights of victims in international criminal law.",
+    name: 'Carla Fernandez',
+    image: './assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_39309469-stock-photo-beautiful-african-american-student-or.jpg',
+    profession: 'Professor of Law at the University of Madrid',
+    resume: 'Carla specializes in the rights of victims in international criminal law.',
   },
   {
-    name: "Pietro Martinelli",
-    image: "./assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_88396462-stock-photo-successful-senior-man-sitting-at.jpg",
-    profession: "Professor of Law at the University of Rome",
-    resume: "Pietro is a Lecturer in law with an interest in human rights and animal rights.",
+    name: 'Pietro Martinelli',
+    image: './assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_88396462-stock-photo-successful-senior-man-sitting-at.jpg',
+    profession: 'Professor of Law at the University of Rome',
+    resume: 'Pietro is a Lecturer in law with an interest in human rights and animal rights.',
   },
   {
-    image: "./assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_411571758-stock-photo-portrait-senior-businesswoman-wearing-glasses.jpg",
-    name: "Albian Schmith",
-    profession: "Professor of Law at the University of Leicester",
-    resume: "Albian drafted and co-ordinated a legal brief in support the Nonhuman Rights Project.",
+    image: './assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_411571758-stock-photo-portrait-senior-businesswoman-wearing-glasses.jpg',
+    name: 'Albian Schmith',
+    profession: 'Professor of Law at the University of Leicester',
+    resume: 'Albian drafted and co-ordinated a legal brief in support the Nonhuman Rights Project.',
   },
   {
-    image: "./assets/00b66d1ac26662551af857811ed6e9d3.png",
-    picture: "./assets/depositphotos_229020394-stock-photo-beautiful-female-teacher-formal-wear.jpg",
-    name: "Rosa Maestu",
-    profession: "Professor of Law at the University of Santiago de Chile",
-    resume: "Rosa is a Senior Lecturer in the Law School of Santiago de Chile.",
+    image: './assets/00b66d1ac26662551af857811ed6e9d3.png',
+    picture: './assets/depositphotos_229020394-stock-photo-beautiful-female-teacher-formal-wear.jpg',
+    name: 'Rosa Maestu',
+    profession: 'Professor of Law at the University of Santiago de Chile',
+    resume: 'Rosa is a Senior Lecturer in the Law School of Santiago de Chile.',
   },
 ];
 
-speakers.forEach((e,i)=> {
- 
+speakers.forEach((e, i) => {
   const html = `
   <div class=speaker-box>
   <div class=images-box>
@@ -62,31 +61,30 @@ speakers.forEach((e,i)=> {
     <div class="grey-line"></div>
     <p class="resume">${speakers[i].resume}</p>
     </div>
-</div>`
-speakerBoxes.insertAdjacentHTML('beforeend', html);
+</div>`;
+  speakerBoxes.insertAdjacentHTML('beforeend', html);
 });
 
-//Open mobile menu
+// Open mobile menu
 
 const mobileMenu = document.querySelector('.mobile-menu');
 const btnOpenMenu = document.querySelector('.hamburger-menu');
 
-
-const openMenu = function() {
-  mobileMenu.style.display = "flex";
-  btnOpenMenu.style.display = "none";
-}
+const openMenu = function () {
+  mobileMenu.style.display = 'flex';
+  btnOpenMenu.style.display = 'none';
+};
 btnOpenMenu.addEventListener('click', openMenu);
 
-//Close mobile menu
+// Close mobile menu
 
 const btnCloseMenu = document.querySelector('.btn-close-l');
 const linkSection = document.querySelector('.links');
 
-const closeMenu = function() {
-  mobileMenu.style.display = "none";
-  btnOpenMenu.style.display = "block";
-}
+const closeMenu = function () {
+  mobileMenu.style.display = 'none';
+  btnOpenMenu.style.display = 'block';
+};
 
-btnCloseMenu.addEventListener("click", closeMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
 linkSection.addEventListener('click', closeMenu);
